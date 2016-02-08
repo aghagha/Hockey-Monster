353d2255d09fd4550c37bc7058ee77e93bb33578
aghagha <amrikudou@gmail.com>
2016-02-08 15:40:12 +0700

    init push+ core game

---
+++ Script/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn" ,1f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn(){
+		Vector3 pos = new Vector3(Random.Range(-7.7f,6.6f),2.35f,2.8f);
+		Instantiate(bullet,pos,Quaternion.identity);
+	}
+}
---
+++ Script/Mainmenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mainmenu : MonoBehaviour {
+
+	public string startLevel;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayGame(){
+		Application.LoadLevel(startLevel);
+	}
+}
