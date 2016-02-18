33fe0fbbd1a81b7c8d5f23522092719a06d17fec
aghagha <amrikudou@gmail.com>
2016-02-18 16:34:58 +0700

    Recovery Repo

---
+++ Script/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    bool isDead = false;
+
+    public float hp;
+    public GameObject playerAvatar;
+    public Slider healthBar;
+
+	// Use this for initialization
+
+    void Awake()
+    {
+        hp = 1000f;
+        healthBar.maxValue = hp;
+        healthBar.value = hp;
+    }
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "bullets")
+        {
+            float damage = col.gameObject.GetComponent<Bullet>().damage;
+            float force = col.gameObject.GetComponent<Bullet>().currMag;
+            float totalDamage = damage + force;
+            hp -= totalDamage;
+            healthBar.value = hp;
+            print("You took " + totalDamage + " damage!");
+            Destroy(col.gameObject);
+            if(hp <= 0)
+            {
+                hp = 0;
+                healthBar.value = 0;
+                Destroy(playerAvatar);
+                isDead = true;
+            }
+        } else
+        {
+            print("sek salah");
+        }
+    }
+}
